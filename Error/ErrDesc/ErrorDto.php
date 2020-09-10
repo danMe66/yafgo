eae994a498cb863684975b9791d9d8f4f8f3f951
liudandan <dawson2@qq.com>
2020-09-10 16:38:28 +0800

    add ErrorDto.php

--- Error/ErrDesc/ErrorDto.php
+++ Error/ErrDesc/ErrorDto.php
@@ -9,6 +9,7 @@ class Container_Error_ErrDesc_ErrorDto
     const PARAM_FORMAT_REQ_ERROR = '200001_参数无效或者参数格式不规范';
     const PARAM_FORMAT_RES_ERROR = '200002_接口返回必须是一个数组';
     const USER_NOT_LOGIN = '200003_请先登录';
+    const NOT_ROUTE = '200004_接口不存在,请检查地址是否正确';
 
     const SERVICE_NOT_AVAILABLE = '300001_当前服务不可用';
 }
